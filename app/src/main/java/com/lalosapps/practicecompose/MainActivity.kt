1a283b8b3f53a05ce373f0e52553890a90f890ae
cbarrios <lalobarrios16@yahoo.com>
2022-05-16 22:27:58 -0400

    master: (2) Compose article

--- app/src/main/java/com/lalosapps/practicecompose/MainActivity.kt
+++ app/src/main/java/com/lalosapps/practicecompose/MainActivity.kt
@@ -1,43 +1,78 @@
 package com.lalosapps.practicecompose
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.lalosapps.practicecompose.ui.theme.PracticeComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PracticeComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MyArticle()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyArticle() {
+    PracticeComposeTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            ArticleContent()
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    PracticeComposeTheme {
-        Greeting("Android")
+fun ArticleContent() {
+    Column {
+        Image(
+            painter = painterResource(R.drawable.bg_compose_background),
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth
+        )
+        Text(
+            text = stringResource(R.string.title),
+            fontSize = 24.sp,
+            modifier = Modifier.padding(all = 16.dp)
+        )
+        Text(
+            text = stringResource(R.string.body_short),
+            modifier = Modifier.padding(horizontal = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = stringResource(R.string.body_long),
+            modifier = Modifier.padding(all = 16.dp),
+            textAlign = TextAlign.Justify
+        )
     }
+}
+
+@Preview
+@Preview(uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun MyArticlePreview() {
+    MyArticle()
 }
